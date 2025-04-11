90dc0829bddda42998571be0ac40c3be229ef6a7
rafisr23 <rafiridwan23@gmail.com>
2025-04-11 23:48:04 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,12 @@ use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('login');
-})->name('home')->middleware('guest');
+})->name('home');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
-
-Route::middleware('auth')->group(function () {
-    Route::get('/home', [HomeController::class, 'index'])->name('index');
-
-    Route::get('/profile', [ProfileController::class, 'showprofileForm'])->name('profile');
-    Route::get('/profile/pdf/{id}', [ProfileController::class, 'exportToPDF'])->name('exportpdf');
-
-    Route::put('/change-password', [AuthController::class, 'update'])->name('password.update');
-
-    // Route::post('/getUserId', [HomeController::class, 'getUserId'])->name('getUserId');
-    Route::post('/get-tweet-user', [HomeController::class, 'getTweetUser'])->name('getTweetUser');
-
-});
 
 Route::middleware('auth')->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('index');
